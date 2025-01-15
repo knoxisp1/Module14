ef284171291e6e547cdeb93ea896fcbd8bd29983
Eduard <knoxwillesp@gmail.com>
2025-01-15 21:17:45 +0300

    Update module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -5,25 +5,22 @@ cursor = connection.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users(
-id  INTEGER PRIMARY KEY,
+id INTEGER PRIMARY KEY,
 username TEXT NOT NULL,
 email TEXT NOT NULL,
 age INTEGER,
 balance INTEGER NOT NULL
 )
-
-
-
 ''')
- for i in range(1, 11):
-    cursor.execute("INSERT INTO Users(username,email,age,balance) VALUES (?,?,?,?)",
-                  (f"User{i}", f"example{i}@gmail.com", f"{i}0", "1000"))
- cursor.execute("UPDATE Users SET balance=500 WHERE id%2=1")
- cursor.execute("DELETE FROM Users   WHERE id%3=1")
+for i in range(1,11):
+  cursor.execute("INSERT INTO Users(username,email,age,balance) VALUES (?,?,?,?)",(f"user{i}",f"example{i}@gmail.com",f"{i}0","1000"))
+cursor.execute("UPDATE Users SET balance=500 WHERE id%2=1")
+cursor.execute("DELETE FROM Users WHERE id%3=1 ")
 cursor.execute("SELECT username,email,age,balance FROM Users WHERE age!=60")
 users = cursor.fetchall()
 for user in users:
-    print(f"Имя:{user[0]} |Почта:{user[1]}|Возраст:{user[2]}|Баланс:{user[3]}")
+    print(f"Имя:{user[0]}|Почта:{user[1]}|Возраст:{user[2]}|Баланс:{user[3]}")
 
 connection.commit()
 connection.close()
+
