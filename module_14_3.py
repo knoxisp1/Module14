1238adde44c462ea3722949c0b816f5d67d56cc7
Eduard <knoxwillesp@gmail.com>
2025-01-21 23:04:43 +0300

    Update module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -1,11 +1,9 @@
-from aiogram import Bot, Dispatcher, types, executor
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher import FSMContext
 import asyncio
-import os
 
 api = ""
 bot = Bot(token=api)
@@ -21,7 +19,6 @@ kb_inline = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Product2", callback_data="product_buying")],
     [InlineKeyboardButton(text="Product3", callback_data="product_buying")],
     [InlineKeyboardButton(text="Product4", callback_data="product_buying")]
-
 ])
 
 
@@ -33,26 +30,28 @@ class UserState(StatesGroup):
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    await message.answer("Привет! Я бот помогающий твоему здоровью", reply_markup=kb_reply)
+    await message.answer("Привет! Я бот помогающий твоему здоровью.", reply_markup=kb_reply)
 
 
 @dp.message_handler(text="Купить")
 async def get_buying_list(message):
-    for j in range(1, 5):
-        await message.answer(f"Название:Product{j}|Описание:описание{j}|Цена:{j * 100}")
-        with open(f'Images/{j}.png', "rb") as file1:
-            await message.answer_photo(file1)
+    for i in range(1, 5):
+        await message.answer(f"Название:Product {i}|Описание: Описание{i}|Цена:{i * 100}")
+        with open(f'C:/Users/Edward/PycharmProjects/Module14/Images/{i}.png', "rb") as img:
+            await message.answer_photo(img)
 
     await message.answer("Выберите продукт для покупки:", reply_markup=kb_inline)
 
+
 @dp.callback_query_handler(text="product_buying")
 async def send_confirm_message(call):
-    await call.message.answer("Вы успешно приобрели продукт.")
+    await call.message.answer("Вы успешно приобрели продукт!")
     await call.answer()
 
+
 @dp.message_handler()
-async def all_messages(message):
-    await message.answer("Введите команду /start, что бы начать общение")
+async def all_message(message):
+    await message.answer("Введите команду /start, что бы начать общение.")
 
 
 if __name__ == "__main__":
